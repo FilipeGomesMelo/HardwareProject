bbb7a54beac2e095d841f625d5b98ca558ab4dcc
FilipeGomesMelo <61971614+FilipeGomesMelo@users.noreply.github.com>
2021-11-06 23:16:05 -0300

    Correção do TB do WD

--- parts_made/Test_Benches/mux_WD_TB.v
+++ parts_made/Test_Benches/mux_WD_TB.v
@@ -3,10 +3,10 @@
 module mux_WD_TB();
 
 	reg [2:0] selector_TB;
-    wire [31:0] data_0_TB, data_1_TB, data_2_TB, data_3_TB, data_4_TB, data_5_TB, data_6_TB;
+    wire [31:0] data_0_TB, data_1_TB, data_2_TB, data_3_TB, data_4_TB, data_5_TB;
     wire [31:0] data_out_TB;
 
-	mux_WD DUT(selector_TB, data_0_TB, data_1_TB, data_2_TB, data_3_TB, data_4_TB, data_5_TB, data_6_TB, data_out_TB);
+	mux_WD DUT(selector_TB, data_0_TB, data_1_TB, data_2_TB, data_3_TB, data_4_TB, data_5_TB, data_out_TB);
 
     assign data_0_TB = 32'd0;
     assign data_1_TB = 32'd1;
@@ -14,7 +14,6 @@ module mux_WD_TB();
     assign data_3_TB = 32'd3;
     assign data_4_TB = 32'd4;
     assign data_5_TB = 32'd5;
-    assign data_6_TB = 32'd6;
 
 	initial
 		begin
