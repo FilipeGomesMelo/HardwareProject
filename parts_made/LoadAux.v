d59b8a736c33a8bc16a115f71189aa864e5ef207
FilipeGomesMelo <61971614+FilipeGomesMelo@users.noreply.github.com>
2021-11-08 22:27:04 -0300

    Update LoadAux.v

--- parts_made/LoadAux.v
+++ parts_made/LoadAux.v
@@ -1,12 +1,12 @@
 module StoreAux (
     input wire [1:0] selector,
-    input wire [31:0] data_0, data_1,
+    input wire [31:0] data_in,
     output wire [31:0] data_out
 );
 
-    assign data_out = (selector == 2'b00) ? data_0 :
-                      (selector == 2'b01) ? {16'b0, data_0[15:0]} :
-                      (selector == 2'b10) ? {24'b0, data_0[7:0]} :
+    assign data_out = (selector == 2'b00) ? data_in :
+                      (selector == 2'b01) ? {16'b0, data_in[15:0]} :
+                      (selector == 2'b10) ? {24'b0, data_in[7:0]} :
                       32'dx;
 
 endmodule
