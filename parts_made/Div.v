1b0888e3a976a0ea30c5c128acaa9f6d3afe6789
FilipeGomesMelo <61971614+FilipeGomesMelo@users.noreply.github.com>
2021-11-24 00:12:37 -0300

    Módulo de divisão e mudanças de estilo

---
+++ parts_made/Div.v
@@ -0,0 +1,67 @@
+module Div (input wire clk, reset, resetlocal,
+            input wire [31:0] Divisor, Dividendo,
+            output reg ZeroDivision,
+            output reg [31:0] Hi, Lo
+);
+
+    reg [31:0] divs, divd;
+    reg [31:0] quociente, resto;
+
+    reg [5:0] digito_atual;
+
+    always @ (posedge clk) begin
+        if (reset == 1'b1 | resetlocal == 1'b1) begin
+            if (Divisor == 32'b0) begin
+                ZeroDivision = 1'b1;
+            end else begin
+                if (Divisor[31] == 1'b1) begin
+                    divs = ~Divisor + 1'b1;
+                end else begin
+                    divs = Divisor;
+                end
+
+                if (Dividendo[31] == 1'b1) begin
+                    divd = ~Dividendo + 1'b1;
+                end else begin
+                    divd = Dividendo;
+                end
+
+                ZeroDivision = 1'b0;
+                digito_atual = 6'd32;
+                quociente = 32'b0;
+                resto = 32'b0;
+            end
+        end else if (digito_atual != 6'd000000 && !ZeroDivision) begin
+            resto = resto << 1;
+            resto[0] = divd[digito_atual-1];
+
+            if (resto >= divs) begin
+                resto = resto - divs;
+                quociente[digito_atual-1] = 1'b1;
+            end
+
+            digito_atual = digito_atual - 1'b1;
+            if (digito_atual == 6'd000000) begin
+                if (Dividendo[31] != Divisor[31]) begin
+                    Lo = -(quociente+1'b1);
+                end else begin
+                    Lo = quociente;
+                end
+
+                if (Dividendo[31] != Divisor[31]) begin
+                    if (Divisor[31] == 1'b1) begin
+                        Hi = -(divs-resto);
+                    end else begin
+                        Hi = divs-resto;
+                    end
+                end else begin
+                    if (Divisor[31] == 1'b1) begin
+                        Hi = -resto;
+                    end else begin
+                        Hi = resto;
+                    end
+                end
+            end
+        end
+    end
+endmodule
--- parts_made/Mult.v
+++ parts_made/Mult.v
@@ -2,40 +2,36 @@ module Mult (input wire clk, reset, resetlocal,
             input wire [31:0] A, B,
             output reg [31:0] Hi, Lo
 );
-reg [31:0] M, a, Q;
-reg Q0;
-reg [5:0] N;
-reg [31:0] ComplementoM;
+	reg [31:0] M, a, Q;
+	reg Q0;
+	reg [5:0] N;
+	reg [31:0] ComplementoM;
 
-always @ (posedge clk) 
-begin
-    if (reset == 1'b1 | resetlocal == 1'b1)
-    begin
-      M = A;
-      Q = B;
-      a = 32'b0;
-      Q0 = 0;
-      N = 6'd32;
-      ComplementoM = ~M + 1'b1;
-    end else if (N != 6'd0) begin
-      if (Q0 == 1'b1 && Q[0] == 1'b0)
-      begin
-        a = a + M;
-      end
-      else if (Q0 == 1'b0 && Q[0] == 1'b1)
-      begin
-        a = ComplementoM + a;
-      end
-      {a,Q,Q0} = {a,Q,Q0} >>> 1'b1;
-      if (a[30] == 1'b1) begin
-        a[31] = 1'b1;
-      end
-      N = N - 1'b1;
-      if (N == 6'd0)
-      begin
-        Hi = a;
-        Lo = Q;
-      end
-    end
-end
+	always @ (posedge clk) begin
+		if (reset == 1'b1 | resetlocal == 1'b1) begin
+			M = A;
+			Q = B;
+			a = 32'b0;
+			Q0 = 0;
+			N = 6'd32;
+			ComplementoM = ~M + 1'b1;
+		end else if (N != 6'd0) begin
+			if (Q0 == 1'b1 && Q[0] == 1'b0) begin
+				a = a + M;
+			end else if (Q0 == 1'b0 && Q[0] == 1'b1) begin
+				a = ComplementoM + a;
+			end
+			
+			{a,Q,Q0} = {a,Q,Q0} >>> 1'b1;
+			if (a[30] == 1'b1) begin
+				a[31] = 1'b1;
+			end
+			
+			N = N - 1'b1;
+			if (N == 6'd0) begin
+				Hi = a;
+				Lo = Q;
+			end
+		end
+	end
 endmodule
